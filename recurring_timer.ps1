b970d252b4f3ef05244ad5349928b3d192eae095
ajaytomgeorge <ajaytomgeorge@users.noreply.github.com>
2020-11-04 15:34:43 +0530

    Time Updated

--- recurring_timer.ps1
+++ recurring_timer.ps1
@@ -1,27 +1,28 @@
 $ErrorActionPreference = "Stop"
 $counter = 0;
 for (; ; ) {
-    if($counter -eq 0) {
+    if ($counter -eq 0) {
         $counter += 1
         $notificationTitle = "Hai Ajay, Startup Protocol BASIC Initiated at " + [DateTime]::Now.ToShortTimeString()
-     }else {
+    }
+    else {
         $notificationTitle = "Hai Ajay, Time for a break: " + [DateTime]::Now.ToShortTimeString() + "`nCurrent Protocol BASIC"
-     }
+    }
 
     try {
-       
+
 
         [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
         $template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText01)
-        
+
         #Convert to .NET type for XML manipuration
         $toastXml = [xml] $template.GetXml()
         $toastXml.GetElementsByTagName("text").AppendChild($toastXml.CreateTextNode($notificationTitle)) > $null
-        
+
         #Convert back to WinRT type
         $xml = New-Object Windows.Data.Xml.Dom.XmlDocument
         $xml.LoadXml($toastXml.OuterXml)
-        
+
         $toast = [Windows.UI.Notifications.ToastNotification]::new($xml)
         $toast.Tag = "PowerShell"
         $toast.Group = "PowerShell"
@@ -31,9 +32,9 @@ for (; ; ) {
         $notifier.Show($toast);
     }
     catch {
-       Write-Host "Unexpected Exception occured"
+        Write-Host "Unexpected Exception occured"
     }
-   
+
     # wait for a minute
-    Start-Sleep 900
+    Start-Sleep 600
 }
